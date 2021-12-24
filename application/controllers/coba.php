6e5fc1abdca4875329eecc154d0c205289938096
MhmdAgilFrdynsyh <muhamadagilferdiyansyah@gmail.com>
2021-12-24 19:23:54 +0700

    applikasi PMB

---
+++ application/controllers/coba.php
@@ -0,0 +1,10 @@
+<?php
+
+class coba extends CI_controller {
+  public function index()
+  {
+    echo "<h1>Perkenalkan</h1>";
+    echo "Nama Saya Muhamad Agil Ferdiyansyah, saya tinggal di rumah dong";
+}
+}
+ ?>
---
+++ application/controllers/cobalagi.php
@@ -0,0 +1,10 @@
+<?php
+
+class cobalagi extends CI_controller{
+  public function index()
+  {
+    $this->load->view('Welcome_slur');
+  }
+}
+
+ ?>
